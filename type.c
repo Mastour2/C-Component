68a35afd07c4990ab1fb74a5c8a9ba7be5f2738d
Mastour <49590405+Mastour2@users.noreply.github.com>
2024-08-15 03:58:33 +0300

    Create type.c

---
+++ type.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// size 1 bite * length 
+typedef char* string;
+
+// size 8 bits
+typedef signed char i8;
+
+// size 16 bits
+typedef short int i16;
+
+// size 32 bits
+typedef long i32;
+
+// size 64 bits
+typedef long long i64;
+
+// size 64bit or 32bit
+typedef size_t usize;
+
+typedef enum {
+   false, true
+} bool;
